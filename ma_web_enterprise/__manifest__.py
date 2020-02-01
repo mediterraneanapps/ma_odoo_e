8a726830d66b0f78240df04c9183989727a05c5a
mediterraneanapps <58656212+mediterraneanapps@users.noreply.github.com>
2020-02-01 14:47:57 +0100

    Add files via upload

---
+++ ma_web_enterprise/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Theme Web Enterprise',
+    'category': 'Themes/Backend',
+    'version': '1.0',
+    'description':
+        """
+Theme Enterprise Web Client.
+===========================
+
+This module modifies the web addon to provide Enterprise design and responsiveness.
+        """,
+    "author": "Mediterranean Apps",
+    "price": "49",
+    "currency": "EUR",
+    "support": "mediterranean.apps@gmail.com",
+    'depends': ['web'],
+    'auto_install': True,
+    'data': [
+        'views/webclient_templates.xml',
+    ],
+    'qweb': [
+        "static/src/xml/*.xml",
+    ],
+     "license": "OPL-1",
+}
